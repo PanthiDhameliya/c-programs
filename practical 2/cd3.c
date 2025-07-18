7c6608ba7e5509f4555a45b29e567219dd0fddbb
8460241894 <dhameliyapanthi311@gmail.com>
2025-07-18 16:07:00 +0530

    Add files via upload

---
+++ practical 2/cd3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+        int length,breadth,area,perimeter;
+        length=70;
+        breadth=90;
+        area=length*breadth;
+        perimeter=2*(length+breadth);
+        printf("area of a the park= %d\n",area);
+        printf("perimeter= %d",perimeter);
+        return 0;
+
+}
