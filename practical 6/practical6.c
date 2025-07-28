0500fa29831a85ef45d5d4be8d85a4d216f446c0
8460241894 <dhameliyapanthi311@gmail.com>
2025-07-28 15:21:34 +0530

    Add files via upload

---
+++ practical 6/practical6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float celsius,Fahrenheit;
+    printf("Enter the Temperatures in celsius=");
+    scanf("%f",&celsius);
+    Fahrenheit=(celsius*9.0/5.0)+32;
+    printf("The Temperatures in Fahrenheit=%f",Fahrenheit);
+
+}
+
